6d60bfbdd197c3223c04def251739562354c8991
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2017-12-05 19:18:31 +0100

    feat(Day05): Solve part two
    Ooops, i forgot to clone the input before running solvers.

--- days/05/index.js
+++ days/05/index.js
@@ -8,6 +8,5 @@ const txt = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'UTF-8')
 
 const array = txt.split(/\n/);
 
-console.info('Answer part one: ', solverOne(array));
-console.info('Answer part two: ', solverTwo(array));
-
+console.info('Answer part one: ', solverOne(array.slice()));
+console.info('Answer part two: ', solverTwo(array.slice()));
