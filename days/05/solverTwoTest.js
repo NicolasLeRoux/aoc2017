3d56e02bc6b40dacb5a2d24c24209c19d7c65a99
NicolasLeRoux <mr.leroux.nicolas@gmail.com>
2017-12-05 09:36:34 +0100

    feat(Day05): Small step forward on part two

---
+++ days/05/solverTwoTest.js
@@ -0,0 +1,21 @@
+const expect = require('expect.js');
+const solve = require('./solverTwo.js');
+
+describe.only('Day 5: A Maze of Twisty Trampolines, All Alike (Part two),', function() {
+	/**
+	 * Example n°01: In this example, the exit is reached in 5 steps.
+	 */
+	describe('Example n°01,', function() {
+		it('should return 10 for the given input.', function() {
+			var array = [
+				'0',
+				'3',
+				'0',
+				'1',
+				'-3'
+			];
+
+			expect(solve(array)).to.equal(10);
+		});
+	});
+});
