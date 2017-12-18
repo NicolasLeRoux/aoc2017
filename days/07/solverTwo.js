fe3264923cc181b38e93c99e1f51126b71564900
NicolasLeRoux <mr.leroux.nicolas@gmail.com>
2017-12-18 17:45:51 +0100

    feat(Day07): Small step forward solving part two

--- days/07/solverTwo.js
+++ days/07/solverTwo.js
@@ -1,11 +1,45 @@
 const utils = require('./utils.js');
 
 module.exports = function (array) {
-	let struct = utils.buildCircusTower(array),
-		childs = struct.childs;
+	let node = utils.buildCircusTower(array),
+		parentNode,
+		childIndex;
 
-	let tmp = childs.map(child => utils.calculBalance(child));
-	console.log(tmp);
+	while (true) {
+		let balances = node.childs.map(child => {
+			return utils.calculBalance(child);
+		});
+
+		if (balances.length > 2) {
+			let sorted= balances.slice()
+					.sort((a, b) => a < b ? 1 : -1),
+				index;
+
+			if (sorted[0] !== sorted[1]) {
+				index = balances.indexOf(sorted[0]);
+			} else if (sorted[sorted.length - 2] !== sorted[sorted.length - 1]) {
+				index = balances.indexOf(sorted[sorted.length - 1]);
+			}
+
+			if (index !== undefined) {
+				parentNode = node;
+				childIndex = index;
+				node = parentNode.childs[index];
+			} else break;
+		} else {
+			break;
+		}
+	}
+
+	let incorrectBalance = utils.calculBalance(node),
+		correctIndex = childIndex !== 0 ? 0 : 1,
+		correctBalance = utils.calculBalance(parentNode.childs[correctIndex]),
+		diff = correctBalance - incorrectBalance;
+
+	if (node.childs.length !== 2) {
+	} else {
+		console.log(node.weight - diff);
+	}
 
 	return 0;
 };
