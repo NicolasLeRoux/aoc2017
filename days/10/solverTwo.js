318a81b1b4e2e6a416f67e1d00405548c209bd87
NicolasLeRoux <mr.leroux.nicolas@gmail.com>
2017-12-19 09:43:11 +0100

    feat(Day10): Small step forward solving part two maze

--- days/10/solverTwo.js
+++ days/10/solverTwo.js
@@ -1,15 +1,29 @@
 const utils = require('./utils.js');
 
 module.exports = function (str) {
-	let array = utils.toASCII(str);
-	array = utils.addStandardSuffix(array);
+	let array = utils.toASCII(str),
+		steps = utils.addStandardSuffix(array),
+		currentPosition = 0,
+		skipSize = 0,
+		hashList = new Array(256)
+			.fill(0)
+			.map((val, i) => i);
 
-	let hashList = utils.knotHashList(256, array);
-	// 64 rounds
+	for (let j = 0; j <= 64; j++) {
+		for (let i = 0; i < steps.length; i++) {
+			let step = +steps[i],
+				to = utils.circularAdd(currentPosition, step, hashList.length),
+				subList = utils.circularSlice(hashList, currentPosition, to);
+
+			if (step !== 0) {
+				hashList = utils.circularReplace(hashList, subList.reverse(), currentPosition);
+			}
+
+			currentPosition = utils.circularAdd(currentPosition, step + skipSize, hashList.length);
+			skipSize++;
+		}
+	}
 
 	let denseHash = utils.reduceSparseHash(hashList);
-
-	// ToHexa
-
-	return '';
+	return utils.toHexa(denseHash);
 };
