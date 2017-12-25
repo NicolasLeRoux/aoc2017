c6b20e0b504a6bc111d5a94caa3f999854b51afc
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2017-12-25 12:52:59 +0100

    feat(Day25): Add runner for day 25 maze

---
+++ days/25/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+const solverOne = require('./solverOne.js');
+
+const txt = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'UTF-8')
+	.replace(/\n$/, '');
+
+const array = txt.split(/\n\n/);
+
+console.info('Answer part one: ', solverOne(array));
